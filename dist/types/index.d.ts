3d717949d1c46670d8f91cea05998285c38983ed
wuwei <wuwei@taqu.cn>
2023-03-22 21:18:36 +0800

    fix: 重复修改

---
+++ dist/types/index.d.ts
@@ -0,0 +1,7 @@
+import * as icons from "./component.js";
+declare const _default: (app: any, { prefix }?: {
+    prefix?: string | undefined;
+}) => void;
+export default _default;
+export { icons };
+export * from "./component.js";
---
+++ vite-env.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="vite/client" />
+declare module "App.vue"
+declare module "*.vue" {
+  import { App, defineComponent } from "vue"
+  const component: ReturnType<typeof defineComponent> & {
+    install(app: App): void
+  }
+  export default component
+}
+declare module '*.md' {
+  import type { ComponentOptions } from 'vue'
+  const Component: ComponentOptions
+  export default Component
+}
+declare module "axios"
+declare module "wwcattleitem"
+declare module "@wwcattlewei/icons-vue/global"
+
+declare module "@kangc/v-md-editor"
+declare module "prismjs"
+declare module "@kangc/v-md-editor/lib/theme/vuepress.js"
+declare module "component.js"
