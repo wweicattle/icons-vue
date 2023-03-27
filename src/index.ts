fe942816d26ba7ae88fa32e8c84272fbce0d2ea9
wuwei <wuwei@taqu.cn>
2023-03-27 17:26:38 +0800

    feat: 修改index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-
 import * as icons from "./component"
-import type { App } from 'vue'
+import type { App } from "vue"
 
 export interface InstallOptions {
   /** @default `Tq` */
   prefix?: string
 }
-const instal={
-    install:(app: App, { prefix = '' }: InstallOptions = {}) => {
-      for (const [key, component] of Object.entries(icons)) {
-        app.component(prefix + key, component)
-      }
-      return app
+const instal = {
+  install: (app: App, { prefix = "" }: InstallOptions = {}) => {
+    for (const [key, component] of Object.entries(icons)) {
+      app.component(prefix + key, component)
     }
+    return app
+  },
+} as {
+  install: () => App
 }
 export default instal
 
-export * from './component'
+export * from "./component"
