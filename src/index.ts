1b0f2fdf86c80127d4e11b4f588d72f9b459bf8e
wuwei <wuwei@taqu.cn>
2023-03-27 15:07:33 +0800

    修改入口路径

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-import * as icons from "./components"
+import * as icons from "./components.js"
 import type { App } from 'vue'
 
 export interface InstallOptions {
@@ -13,4 +13,4 @@ export default (app: App, { prefix = 'ElIcon' }: InstallOptions = {}) => {
 }
 
 export { icons }
-export * from './components'
+export * from './components.js'
