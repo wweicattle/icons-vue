0e3f2c5ac1c879a6d45bc704059c7b016563b1cd
wuwei <wuwei@taqu.cn>
2023-04-06 21:50:29 +0800

    fix:repeat once

--- es/index.js
+++ es/index.js
@@ -1,12 +1,3 @@
 /*! Icons Vue v2.0.3 */
 
 import*as o from"./component.js";export*from"./component.js";const r={install:(t,{prefix:n=""}={})=>{for(const[e,p]of Object.entries(o))t.component(n+e,p);return t},icons:o};var s=r;export{s as default};
-.component(prefix + key, component);
-    return app;
-  },
-  icons
-};
-var src_default = IconsData;
-export {
-  src_default as default
-};
